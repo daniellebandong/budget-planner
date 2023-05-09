7103bad479a8701fca4f5dd71ad3d0cc81194c79
daniellebandong <46451872+daniellebandong@users.noreply.github.com>
2023-05-09 17:13:29 -0600

    tested handle change methods

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,6 @@
-const SubmitButton = () => {
+const SubmitButton = (props :any) => {
     return ( 
-        <button className="p-2 border border-green-600 bg-green-600 rounded-lg text-white ">Submit</button>
+        <button className="p-2 border border-green-600 bg-green-600 rounded-lg text-white " onClick={props.onClick}>Submit</button>
      );
 }
  
--- src/components/forms/BudgetEntry.tsx
+++ src/components/forms/BudgetEntry.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, SetStateAction, useState} from 'react';
 import { SubmitButton } from '../buttons/Button';
 
 
@@ -6,31 +6,41 @@ const BudgetEntry = () => {
     const [desc, setDesc] = useState<string>("");
     const [date, setDate] = useState("");
     const [category, setCategory] = useState([]);
+    const [amount, setAmount] = useState(0);
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setDate(event.target.value)
-        console.log(date)
+        event.preventDefault();
+        setDesc(desc)
+        setDate(date)
+        setCategory(category)
+        setAmount(amount)
     };
 
     return ( 
         <form className='' action="">
             <div className='p-1 border border-black grid gap-4 md:flex'>
                 <div className=''>
-                    <label className='block' htmlFor="desc">Description:</label>
-                    <input className='border w-full md:' id="desc" type="text" placeholder="Description" onChange={handleChange}/>
+                    <label className='block' htmlFor="desc">Description</label>
+                    <input className='border w-full md:' onChange={(e :React.FormEvent<HTMLInputElement>)=> setDesc(e.currentTarget.value.trim())} id="desc" type="text" placeholder="Description" />
                 </div>
                 <div>
-                    <label className='block' htmlFor="date">Date of Transaction:</label>
-                    <input className='border  w-full md:' id="date" type="date" onChange={handleChange}/>
+                    <label className='block' htmlFor="date">Date of Transaction</label>
+                    <input className='border  w-full md:' id="date" type="date" onChange={(e :React.FormEvent<HTMLInputElement>)=> setDate(e.currentTarget.value.trim())}/>
                 </div>
                 <div>
-                    <label className='block' htmlFor="category">Type of Transaction: </label>
-                    <select className='border w-full md:' name="category" id="category">
+                    <label className='block' htmlFor="category">Type of Transaction </label>
+                    <select className='border w-full md:' name="category" id="category" onChange={(e :React.FormEvent<HTMLSelectElement>)=> setDate(e.currentTarget.value.trim())}>
                         <option value="#">Select an Expense...</option>
+                        <option value="income">Income</option>
+                        <option value="rent">Rent</option>
                     </select>
                 </div>
                 <div>
-                    <SubmitButton/>
+                    <label className='block' htmlFor="amount">Amount</label>
+                    <input className='border  w-full md:' id="amount" type="number" onChange={(e : React.ChangeEvent<HTMLInputElement> )=> setAmount(e.target.value.trim())}/>
+                </div>
+                <div>
+                    <SubmitButton onClick={handleChange}/>
                 </div>
             </div>
             
